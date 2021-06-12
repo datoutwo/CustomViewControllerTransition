05685cb166db9ef946ff1d39185c01037e187dcd
datoutwo <datoutwo@gmail.com>
2021-06-12 10:43:36 +0200

    assignee test

--- CustomViewControllerTransition/AppDelegate.swift
+++ CustomViewControllerTransition/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate {
     private func showMenuViewController() {
-        // ADD TEST
         let menuDetailViewControllerFactory = MenuDetailViewControllerFactory()
         let infoViewControllerFactory = InfoViewControllerFactory()
         let factory = MenuFactory(
